088fbc18684027ddd8ba238ad181d3653a6b9493
金牌辅助 <bubbletease@jinpaifuzhudeMacBook-Air.local>
2025-10-03 10:31:46 -0700

    Add Procfile and changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 import random
 
 app = Flask(__name__)
@@ -170,4 +171,5 @@ def connect_four():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
